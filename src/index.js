c51b00b695548e5c7e6569fe50091a06462ba8f1
Gabriel Chavez <gabosvs@gmail.com>
2022-04-12 18:07:15 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { spawn, removeElement } from './utils.js'
 let installed = false
 
 const containerClasses = [
-  'z-40', 'fixed', 'inset-0', 'flex', 'flex-col', 'flex-col-reverse', 'items-end', 'justify-center',
+  'z-40', 'fixed', 'inset-0', 'flex', 'flex-col', 'flex-col-reverse', 'items-end', 'justify-start',
   'px-4', 'py-6', 'pointer-events-none', 'sm:p-6', 'sm:items-end', 'sm:justify-end'
 ]
 
