6809ff86d879d73e8337b2558dc1f45eb53284ce
Gabriel Chavez <gabosvs@gmail.com>
2022-09-09 20:09:46 -0500

    fix issues

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import { spawn, removeElement } from './utils.js'
 
 let installed = false
 
+const containerClasses = [
+  'z-40', 'fixed', 'inset-0', 'flex', 'flex-col', 'flex-col-reverse', 'items-end', 'justify-start',
+  'px-4', 'py-6', 'sm:p-6',
+]
+
 export default {
   install(Vue) {
     if (installed) return
